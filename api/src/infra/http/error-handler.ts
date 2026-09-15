e41f7eaa8cf09da12fd43eb13d6245beb1a6266b
Anderson Kaiti <anderkaiti@gmail.com>
2026-09-15 18:05:06 -0300

    :sparkles: feat(http): add global error handler with Unauthorized error

---
+++ api/src/infra/http/error-handler.ts
@@ -0,0 +1,25 @@
+import { Unauthorized } from '@errors/unauthorized.ts'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { ZodError, z } from 'zod'
+
+export function errorHandler(
+  error: Error,
+  _request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  if (error instanceof ZodError) {
+    return reply.status(401).send({
+      error: z.treeifyError(error),
+    })
+  }
+
+  if (error instanceof Unauthorized) {
+    return reply.status(401).send({
+      error: error.message,
+    })
+  }
+
+  return reply.status(500).send({
+    error: 'Internal server error',
+  })
+}
